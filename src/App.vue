6c80d7f62ab130202ee62db963d0de0ff3d2b865
Wall <ilnazamirhanov61@gmail.com>
2023-01-20 23:54:02 +0300

    add likes and dislikes methods

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,31 @@
 <template>
   <div>
-    Работает
+    <div>
+      <button v-on:click="addLikes">Like</button>
+      <button v-on:click="addDislike"> Dislike</button>
+    </div>
+    <div>Кол-во лайков: {{ likes }}</div>
+    <div>Кол-во дизлайков: {{ dislikes }}</div>
   </div>
 </template>
 
 <script>
 export default {
-  name: "App"
+  data() {
+    return {
+      likes: 0,
+      dislikes: 5,
+    }
+  },
+  name: "App",
+  methods: {
+    addLikes(){
+      this.likes +=1;
+    },
+    addDislike(){
+      this.dislikes +=1;
+    }
+  }
 }
 </script>
 
