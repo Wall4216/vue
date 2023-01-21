d1b9279abb336bae4375435ef012e1997c278430
Wall <ilnazamirhanov61@gmail.com>
2023-01-22 00:34:07 +0300

    add animation for posts

--- src/components/PostList.vue
+++ src/components/PostList.vue
@@ -1,10 +1,12 @@
 <template>
   <div v-if="posts.length > 0">
   <h3 style="margin-top: 9px">Список</h3>
-  <post-item
-      :post="post"
-      v-for="post in posts" :key="post.id" @remove="$emit('remove', post)">
-  </post-item>
+  <transition-group name="user-list">
+    <post-item
+        :post="post"
+        v-for="post in posts" :key="post.id" @remove="$emit('remove', post)">
+    </post-item>
+  </transition-group>
   </div>
   <h2 v-else style="color: green">
     Список постов пуст
@@ -27,5 +29,20 @@ export default {
 </script>
 
 <style scoped>
-
+.user-list-item {
+  display: inline-block;
+  margin-right: 10px;
+}
+.user-list-enter-active,
+.user-list-leave-active {
+  transition: all 0.4s ease;
+}
+.user-list-enter-from,
+.user-list-leave-to {
+  opacity: 0;
+  transform: translateX(130px);
+}
+.user-list-move {
+  transition: transform 0.4s ease;
+}
 </style>
