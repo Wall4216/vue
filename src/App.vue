0464a171f3b1d3d3f65c69c66613668d8e8d255a
Wall <ilnazamirhanov61@gmail.com>
2023-01-22 01:23:33 +0300

    add app commit

--- src/App.vue
+++ src/App.vue
@@ -2,15 +2,23 @@
 <div class="app">
 
   <h1 style="">Страница с постами</h1>
+  <my-input
+      v-model="searchQuery">
+      placeholder="Поиск"
+  </my-input>
   <div class="app__btns">
     <my-button style="" @click="showDialog">Создать пост</my-button>
   </div>
 <my-select v-model="selectedSord" :options="sortOptions"></my-select>
   <my-dialog v-model:show="dialogVisiable"><PostForm @create="createPost"> </PostForm></my-dialog>
 
-  <PostList v-bind:posts="sortedPosts" v-if="!isPostsLoading" @remove = "removePost"></PostList>
+  <PostList v-bind:posts="sortedAndSearchedPosts" v-if="!isPostsLoading" @remove = "removePost"></PostList>
 <div v-else>Идет загрузка</div>
+<!--  <div class="page__wrapper">
+    <div class="page" v-for="pageNumber in totalpage" :key="pageNumber" :class="{'current-page': page === pageNumber}" @click="changePage(pageNumber)">{{pageNumber}}}</div>
+  </div>-->
 </div>
+  <div ref="observer" class="observer"></div>
 </template>
 
 <script>
@@ -21,8 +29,10 @@ import MyDialog from "@/components/UI/MyDialog";
 import MyButton from "@/components/UI/MyButton";
 import axios from "axios"
 import MySelect from "@/components/UI/MySelect";
+import MyInput from "@/components/UI/MyInput";
 export default {
   components: {
+    MyInput,
     MySelect,
     MyButton,
     MyDialog,
@@ -34,6 +44,10 @@ export default {
       dialogVisiable: false,
       isPostsLoading: false,
       selectedSord: '',
+      searchQuery: '',
+      page: 0,
+      limit: 10,
+      totalpage: 0,
       sortOptions: [
         {value: 'title', name: 'По название'},
         {value: 'body', name: 'По описанию'}
@@ -42,7 +56,11 @@ export default {
   },
   name: "App",
   methods: {
-
+    changePage() {
+      this.page = pageNumber
+      this.$refs(this.$refs.observer)
+      this.fetchsPosts()
+    },
     createPost(post)
     {
       this.posts.push(post);
@@ -61,12 +79,19 @@ export default {
       try {
         this.isPostsLoading = true;
         setTimeout( async () => {
-          const response = await axios.get('https://jsonplaceholder.typicode.com/posts?_limit=10')
+          const response = await axios.get('https://jsonplaceholder.typicode.com/posts', {
+            params: {
+              _page: this.page,
+              _limit: this.limit,
+            }
+          })
+          this.totalpage = Math.ceil(response.headers['x-total-count']/this.limit);
           this.posts = response.data
           this.isPostsLoading = false;
         }, 500)
 
       }
+
       catch (e)
       {
         alert('Ошибка')
@@ -74,17 +99,58 @@ export default {
 
       }
     },
+    async loadMorePosts()
+    {
+      try {
+        this.isPostsLoading = true;
+        setTimeout( async () => {
+          const response = await axios.get('https://jsonplaceholder.typicode.com/posts', {
+            params: {
+              _page: this.page,
+              _limit: this.limit,
+            }
+          })
+          this.totalpage = Math.ceil(response.headers['x-total-count']/this.limit);
+          this.posts = [...this.posts]
+          this.isPostsLoading = false;
+        }, 500)
 
+      }
+
+      catch (e)
+      {
+        alert('Ошибка')
+      } finally {
+
+      }
+    },
   },
   mounted() {
     this.fetchsPosts();
+    let options = {
+      rootMargin: '0px',
+      threshold: 1.0
+    }
+
+    let observer = new IntersectionObserver(callback, options);
+    observer.observe(this.$refs.observer)
   },
   computed: {
     sortedPosts()
     {
       return [...this.posts].sort((post1, post2) => post1(this.selectedSord)?.localeCompare(post[this.selectedSord]))
+    },
+    sortedAndSearchedPosts()
+    {
+      return this.sortedPosts().filter(post => post.title.toLowerCase().includes(this.sortedQuery.toLowerCase()))
     }
   },
+
+  watch: {
+   /* page() {
+      this.fetchsPosts()
+    }*/
+  }
 
 }
 </script>
