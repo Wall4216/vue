e04dd4d689ccacc162fc63f07d68f3a55a669f38
Wall <ilnazamirhanov61@gmail.com>
2023-01-22 17:27:10 +0300

    final correction commit

---
+++ src/pages/About.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <h1>Wall4216</h1>
+    <img src="https://github.com/Wall4216/Wall4216/raw/main/assets/Wall.png">
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped>
+
+</style>
